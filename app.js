725f6ea0395ae637b99921cb746efe87f9af22da
rajxo <93876999+rajxo@users.noreply.github.com>
2024-03-08 18:56:30 +0530

    Update app.js
    i changed logic of calculating rent in this change

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const form=document.querySelector('#trcform');
-const input=document.querySelector('#time');
+const hrsinput=document.querySelector('#time');
+const mininput=document.querySelector('#min');
 const cont=document.querySelector('.container');
 const p=document.querySelector('p');
 
@@ -7,19 +8,16 @@ const p=document.querySelector('p');
 
 form.addEventListener('submit',function(e){
     e.preventDefault();
-    const time=input.value;
-    //console.log(time);
-    //stime(time);
-    // const span=document.createElement('span');
-    // span.innerHTML=<span class="material-symbols-outlined">currency_rupee</span>
-    // p.innerHTML=span
+    const hrs=hrsinput.value;
+    const min=mininput.value;
     
-    p.innerText=`RS. ${stime(time)}`
+    
+    p.innerText=`RS. ${stime(hrs,min)}`
 
 })
 
-function stime(st){
-    const [hourstr,minstr]=st.split(':');
+function stime(hourstr,minstr){
+    //const [hourstr,minstr]=st.split(':');
     //console.log(hourstr,minstr);
     const hrs=parseInt(hourstr,10)||0;
     const mins=parseInt(minstr,10)||0;
